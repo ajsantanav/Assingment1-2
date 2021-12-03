5500740694ad7b5f7ce7286ae42985bea2de43fb
Adrian J. Santana <ajsantanav>
2021-12-03 13:06:02 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,258 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtId = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtNoBoots = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtBoots = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtFullName = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtDaily = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.txtIdOut = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(116, 220)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtName
+        '
+        Me.txtName.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.txtName.Location = New System.Drawing.Point(104, 59)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 23)
+        Me.txtName.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(56, 62)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 91)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 15)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "License or ID:"
+        '
+        'txtId
+        '
+        Me.txtId.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.txtId.Location = New System.Drawing.Point(104, 88)
+        Me.txtId.Name = "txtId"
+        Me.txtId.Size = New System.Drawing.Size(100, 23)
+        Me.txtId.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(37, 120)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 15)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "No Boots:"
+        '
+        'txtNoBoots
+        '
+        Me.txtNoBoots.Location = New System.Drawing.Point(104, 120)
+        Me.txtNoBoots.Name = "txtNoBoots"
+        Me.txtNoBoots.Size = New System.Drawing.Size(100, 23)
+        Me.txtNoBoots.TabIndex = 6
+        Me.txtNoBoots.Text = "0"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(56, 156)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 15)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Boots:"
+        '
+        'txtBoots
+        '
+        Me.txtBoots.Location = New System.Drawing.Point(104, 153)
+        Me.txtBoots.Name = "txtBoots"
+        Me.txtBoots.Size = New System.Drawing.Size(100, 23)
+        Me.txtBoots.TabIndex = 8
+        Me.txtBoots.Text = "0"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(186, 23)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(103, 15)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Snowboard Rental"
+        '
+        'txtFullName
+        '
+        Me.txtFullName.Location = New System.Drawing.Point(353, 58)
+        Me.txtFullName.Multiline = True
+        Me.txtFullName.Name = "txtFullName"
+        Me.txtFullName.Size = New System.Drawing.Size(167, 24)
+        Me.txtFullName.TabIndex = 11
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(306, 62)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(42, 15)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Name:"
+        '
+        'txtDaily
+        '
+        Me.txtDaily.Location = New System.Drawing.Point(354, 117)
+        Me.txtDaily.Name = "txtDaily"
+        Me.txtDaily.Size = New System.Drawing.Size(167, 23)
+        Me.txtDaily.TabIndex = 13
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(284, 117)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(64, 15)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Daily Total:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(354, 88)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(166, 23)
+        Me.TextBox1.TabIndex = 15
+        '
+        'txtIdOut
+        '
+        Me.txtIdOut.AutoSize = True
+        Me.txtIdOut.Location = New System.Drawing.Point(271, 91)
+        Me.txtIdOut.Name = "txtIdOut"
+        Me.txtIdOut.Size = New System.Drawing.Size(77, 15)
+        Me.txtIdOut.TabIndex = 16
+        Me.txtIdOut.Text = "License or ID:"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(388, 193)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 17
+        Me.Button2.Text = "Clear All"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(116, 191)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 18
+        Me.Button3.Text = "Clear Inputs"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(388, 156)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 19
+        Me.Button4.Text = "Clear Inputs"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(590, 376)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.txtIdOut)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtDaily)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtFullName)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtBoots)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtNoBoots)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtId)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtId As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtNoBoots As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtBoots As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtFullName As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtDaily As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents txtIdOut As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,58 @@
+﻿Option Strict Off
+Option Explicit On
+
+Public Class Form1
+
+    Private Sub OurEventHandler(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim button As Button
+        button = CType(sender, Button)
+        Dim holderBoots, holderNoBoots, noBoots, boots, dailyTotal As Double
+
+
+        holderNoBoots = txtNoBoots.Text
+        holderBoots = txtBoots.Text
+
+        noBoots = 20.0 * holderNoBoots
+        boots = 30.0 * holderBoots
+
+        dailyTotal = noBoots + boots
+
+        txtDaily.Text = dailyTotal
+
+        txtFullName.Text = txtName.Text
+        TextBox1.Text = txtId.Text
+
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+        txtNoBoots.Text = "0"
+        txtBoots.Text = "0"
+        txtName.Text = ""
+        txtId.Text = ""
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        txtFullName.Text = ""
+        TextBox1.Text = ""
+        txtDaily.Text = ""
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        txtFullName.Text = ""
+        TextBox1.Text = ""
+        txtDaily.Text = ""
+        txtNoBoots.Text = "0"
+        txtBoots.Text = "0"
+        txtName.Text = ""
+        txtId.Text = ""
+
+    End Sub
+End Class
